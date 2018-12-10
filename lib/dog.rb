97ab5725ed8807d5cb3f845019f94270ecbb6aea
github username <github email address>
2018-12-10 14:32:54 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,10 @@
 class Dog
-  # code goes here
+  # code goes 
+  attr_reader :name #can't be changed
+  attr_accessor :mood # can be changed
+
+  def initialize(name)
+  	@name = name
+  	@mood = "nervous"
+  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,10 @@
 class Fish
   # code goes here
+  attr_reader :name #can't be changed
+  attr_accessor :mood # can be changed
+
+  def initialize(name)
+  	@name = name
+  	@mood = "nervous"
+  end
 end
